52ac3fc59efcfc5fc221b6bc468f1fd1716e7b45
buraktn7 <burak.tuna@hotmail.com>
2025-03-19 12:16:47 +0300

    programcs discount service

--- Services/Discount/MultiShop.Discount/Controllers/DiscountCouponController.cs
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountCouponController.cs
@@ -16,27 +16,31 @@ namespace MultiShop.Discount.Controllers
         {
             _discountService = discountService;
         }
-
+        [HttpGet]
         public async Task<IActionResult> getAllCuopon()
         {
             var values = await _discountService.GetAllCouponAsync();
             return Ok(values);
         }
+        [HttpPost]
         public async Task<IActionResult> CreateCoupon(CreateCouponDto createCouponDto)
         {
             await _discountService.CreateCouponAsync(createCouponDto);
             return Ok();
         }
+        [HttpDelete]
         public async Task<IActionResult> DeleteCoupon(int Id)
         {
             await _discountService.DeleteCouponAsync(Id);
             return Ok();
         }
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetByIdCoupon(int Id)
         {
             var values = await _discountService.GetByIdCouponAsync(Id);
             return Ok (values);
         }
+        [HttpPut]
         public async Task<IActionResult> UpdateCoupon(UpdateCouponDto updateCouponDto)
         {
             await _discountService.UpdateCouponAsync(updateCouponDto);
